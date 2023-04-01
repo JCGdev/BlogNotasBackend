e8ed14f0ee6d0eb693d41630091b94b42954faf5
JCGdev <53711291+JCGdev@users.noreply.github.com>
2023-04-01 13:46:33 +0200

    validationLogin() and addUser() refactored. They now have to parametes: username and password. They also return the instance of the User.
    also added deleteFunction() and throws declarations

--- src/main/java/by/daw/api/db/behaviour/UserDatabaseManager.java
+++ src/main/java/by/daw/api/db/behaviour/UserDatabaseManager.java
@@ -1,6 +1,10 @@
 package by.daw.api.db.behaviour;
 
 import by.daw.api.db.User;
+import by.daw.api.db.exceptions.UserAlreadyExistsException;
+import by.daw.api.db.exceptions.UserDoesNotExistException;
+
+import java.util.List;
 
 public interface UserDatabaseManager {
 
@@ -12,17 +16,40 @@ public interface UserDatabaseManager {
 
 
     /**
-     * Valida un usuario en la BBDD
      *
-     * @param user el objeto Usuario que envuelve el usuario, contraseña e id
-     * @return true si las credenciales coinciden con una entrada en la BBDD
+     * @param username el nombre de usuario
+     * @param password la contraseña del usuario
+     * @return el usuario de la base de datos o null si no se encuentra
      */
-    boolean validateLogin(User user);
+    User validateLogin(String username, String password);
+
     /**
-     * Añade un usuario a la BBDD
-     * @param user el objeto Usuario que envuelve el usuario, contraseña e id
+     * Añade un usuario a la base de datos
+     *
+     * @param username el nombre de usuario
+     * @param password la contraseña
+     * @throws UserAlreadyExistsException si ya existe un usuario con esas credenciales
+     * @return el usuario creado
      */
-    void addUser(User user);
+    User addUser(String username, String password) throws UserAlreadyExistsException;
+
+
+    /**
+     * Elimina a un usuario de la base de datos
+     *
+     * @param username el nombre de usuario
+     * @param password la contraseña del usuario
+     * @throws UserDoesNotExistException si el usuario no existe
+     */
+    void deleteUser(String username, String password) throws UserDoesNotExistException;
+
+
+    /**
+     * Devuelve la lista de usuarios
+     *
+     * @return Una lista conteniendo los usuarios de la BBDD XML
+     */
+    List<User> getUsers();
 
 
 }
